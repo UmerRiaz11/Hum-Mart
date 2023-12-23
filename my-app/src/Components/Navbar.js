2431a1201c49bbbadf707fad2c11778f9e913805
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-12-24 00:27:47 +0500

    working navbar & section

---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div>
+            <div className="nav-bar">
+                <ul className="nav-elements">
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Pages</li>
+                    <li>Vegetables</li>
+                    <li>Blog</li>
+                    <li>Flash Sales</li>
+                </ul>
+                <ul>
+                    <li>Recent View Products</li>
+                </ul>
+            </div>
+            <hr />
+        </div>
+    )
+}
+
---
+++ my-app/src/Components/Section.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Section() {
+    return(
+        <div className="section-img">
+            <img src="../Vegi-pic.png" alt="ERROR" />
+        </div>
+    )
+}
